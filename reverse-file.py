d8c1439f98e671cc3de0a1b609c28d6da6ccc040
azimmsd <azimmasoudi@gmail.com>
2020-12-11 15:36:36 +0330

    added reverse-file.py

---
+++ reverse-file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.9
+
+import argparse
+
+#build the parser
+parser = argparse.ArgumentParser(description='Read a file in reverse')
+parser.add_argument('filename', help='the file to read')
+parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+args = parser.parse_args()
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+    for line in lines:
+        print(line.strip()[::-1])
+#parse the arguments
+
+
+#read the file, reverse the contents and print
