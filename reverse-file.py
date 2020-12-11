7040e3d284eb1f3fc776bc49f9a626c6d57184d2
azimmsd <azimmasoudi@gmail.com>
2020-12-11 16:03:46 +0330

    Added try expect && Added sys.exit

--- reverse-file.py
+++ reverse-file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.9
 
 import argparse
+import sys
 
 #build the parser
 parser = argparse.ArgumentParser(description='Read a file in reverse')
@@ -8,14 +9,20 @@ parser.add_argument('filename', help='the file to read')
 parser.add_argument('--limit', '-l', type=int, help='the number of lines to read')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 args = parser.parse_args()
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
-
-    if args.limit:
-        lines = lines[:args.limit]
-    for line in lines:
-        print(line.strip()[::-1])
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print("Error: {}".format(err))
+    sys.exit(2)
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+        if args.limit:
+            lines = lines[:limit]
+        for line in lines:
+            print(line.strip()[::-1])
 #parse the arguments
 
 
